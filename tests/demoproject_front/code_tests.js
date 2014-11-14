cd232814bb50771ecd68ad19f411e17b245640c4
Alejandro Cirino <cirinoalejandro@gmail.com>
2014-11-14 19:51:06 -0300

    added empty scenario test

--- tests/demoproject_front/code_tests.js
+++ tests/demoproject_front/code_tests.js
@@ -2,14 +2,31 @@ var expect = chai.expect;
 
 describe('Questions', function() {
     
-    before(function(runTests){
-        $.get('http://localhost:8000/drs/demoapp/scenario_1').done(runTests);
+    describe('Questions with no data', function() {
+
+        before(function(runTests){
+            $.get('http://localhost:8000/drs/demoapp/empty_scenario').done(runTests);
+        });
+        
+        it('should get empty questions list', function(done) {
+            (new Questions()).fetch().done(function (data) {
+                expect(data).to.be.empty();
+                done();
+            }).fail(function(err){ done(err); });
+        });
     });
 
-    it('should get the questions list', function(done) {
-        (new Questions()).fetch().done(function (data) {
-            expect(data).to.have.length.of.at.least(4);
-            done();
-        }).fail(function(err){ done(err); });
+    describe('Questions with data', function() {
+
+        before(function(runTests){
+            $.get('http://localhost:8000/drs/demoapp/scenario_1').done(runTests);
+        });
+
+        it('should get the questions list', function(done) {
+            (new Questions()).fetch().done(function (data) {
+                expect(data).to.have.length.of.at.least(4);
+                done();
+            }).fail(function(err){ done(err); });
+        });
     });
 });
