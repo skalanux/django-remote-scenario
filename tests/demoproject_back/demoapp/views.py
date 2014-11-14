93efca6fc70c450d55ed3f1ecb7b4714f8ac345e
Alejandro Cirino <cirinoalejandro@gmail.com>
2014-11-14 19:20:38 -0300

    questions api: render as json

--- tests/demoproject_back/demoapp/views.py
+++ tests/demoproject_back/demoapp/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import HttpResponse
 
 from demoapp.models import Question, Choice
@@ -5,5 +7,6 @@ from demoapp.models import Question, Choice
 # Create your views here.
 def index(request):
     questions = Question.objects.all()
-    return HttpResponse(questions)
+    questions_data = [dict(id=q.id, text=q.question_text) for q in questions]
+    return HttpResponse(json.dumps(questions_data), content_type='application/json')
 
